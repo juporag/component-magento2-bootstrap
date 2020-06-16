bf9ad12e10f55ceb60452dfc029fb0f67fc3aadd
Julio Porter <jporter@onestic.com>
2020-06-16 21:02:09 +0200

    Add file bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+use Magento\Framework\App\Bootstrap;
+require __DIR__ . '/../app/bootstrap.php';
+
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+
+$obj = $bootstrap->getObjectManager();
+
+$state = $obj->get('Magento\Framework\App\State');
+$state->setAreaCode('frontend');
